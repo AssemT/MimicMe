dedac6e9a3d9e8d1b2d689c10149146fc194dc52
AssemT <assemtursyngalyeva@Assems-Macintosh.local>
2016-01-06 00:22:14 +0000

    Initial commit

---
+++ app/src/main/java/com/example/assemtursyngalyeva/mimicme/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.assemtursyngalyeva.mimicme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView tvRegisterLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bLogin:
+                break;
+
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this,RegisterActivity.class));
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/assemtursyngalyeva/mimicme/RegisterActivity.java
@@ -0,0 +1,40 @@
+package com.example.assemtursyngalyeva.mimicme;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etName, etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bRegister:
+                break;
+        }
+    }
+
+}
