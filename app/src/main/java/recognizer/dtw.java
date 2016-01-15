0438452fcb70e8c1cdc785edb8476609bc3560db
AssemT <assemtursyngalyeva@Assems-Macintosh.local>
2016-01-15 00:51:43 +0000

    fff

--- app/src/main/java/recognizer/dtw.java
+++ app/src/main/java/recognizer/dtw.java
@@ -2,141 +2,141 @@ package recognizer;
 
 public class DTW {
 
-        protected float[] seq1;
-        protected float[] seq2;
-        protected int[][] warpingPath;
+    protected float[] seq1;
+    protected float[] seq2;
+    protected int[][] warpingPath;
 
-        protected int n;
-        protected int m;
-        protected int K;
+    protected int n;
+    protected int m;
+    protected int K;
 
-        protected double warpingDistance;
+    protected double warpingDistance;
 
-        //Constructor
-        public DTW(float[] sample, float[] templete) {
-            seq1 = sample;
-            seq2 = templete;
+    //Constructor
+    public DTW(float[] sample, float[] templete) {
+        seq1 = sample;
+        seq2 = templete;
 
-            n = seq1.length;
-            m = seq2.length;
-            K = 1;
+        n = seq1.length;
+        m = seq2.length;
+        K = 1;
 
-            warpingPath = new int[n + m][2];	// max(n, m) <= K < n + m
-            warpingDistance = 0.0;
+        warpingPath = new int[n + m][2];	// max(n, m) <= K < n + m
+        warpingDistance = 0.0;
 
-            this.compute();
+        this.compute();
+    }
+
+    public void compute() {
+        double accumulatedDistance = 0.0;
+
+        double[][] d = new double[n][m];	// local distances
+        double[][] D = new double[n][m];	// global distances
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                d[i][j] = distanceBetween(seq1[i], seq2[j]);
+            }
         }
 
-        public void compute() {
-            double accumulatedDistance = 0.0;
+        D[0][0] = d[0][0];
 
-            double[][] d = new double[n][m];	// local distances
-            double[][] D = new double[n][m];	// global distances
+        for (int i = 1; i < n; i++) {
+            D[i][0] = d[i][0] + D[i - 1][0];
+        }
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    d[i][j] = distanceBetween(seq1[i], seq2[j]);
-                }
-            }
+        for (int j = 1; j < m; j++) {
+            D[0][j] = d[0][j] + D[0][j - 1];
+        }
 
-            D[0][0] = d[0][0];
-
-            for (int i = 1; i < n; i++) {
-                D[i][0] = d[i][0] + D[i - 1][0];
-            }
-
+        for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                D[0][j] = d[0][j] + D[0][j - 1];
+                accumulatedDistance = Math.min(Math.min(D[i-1][j], D[i-1][j-1]), D[i][j-1]);
+                accumulatedDistance += d[i][j];
+                D[i][j] = accumulatedDistance;
             }
+        }
+        accumulatedDistance = D[n - 1][m - 1];
 
-            for (int i = 1; i < n; i++) {
-                for (int j = 1; j < m; j++) {
-                    accumulatedDistance = Math.min(Math.min(D[i-1][j], D[i-1][j-1]), D[i][j-1]);
-                    accumulatedDistance += d[i][j];
-                    D[i][j] = accumulatedDistance;
+        int i = n - 1;
+        int j = m - 1;
+        int minIndex = 1;
+
+        warpingPath[K - 1][0] = i;
+        warpingPath[K - 1][1] = j;
+
+        while ((i + j) != 0) {
+            if (i == 0) {
+                j--;
+            } else if (j == 0) {
+                i--;
+            } else {	// i != 0 && j != 0
+                double[] array = { D[i - 1][j], D[i][j - 1], D[i - 1][j - 1] };
+                minIndex = this.getIndexOfMinimum(array);
+
+                if (minIndex == 0) {
+                    i--;
+                } else if (minIndex == 1) {
+                    j--;
+                } else if (minIndex == 2) {
+                    i--;
+                    j--;
                 }
-            }
-            accumulatedDistance = D[n - 1][m - 1];
-
-            int i = n - 1;
-            int j = m - 1;
-            int minIndex = 1;
-
+            } // end else
+            K++;
             warpingPath[K - 1][0] = i;
             warpingPath[K - 1][1] = j;
+        } // end while
+        warpingDistance = accumulatedDistance / K;
 
-            while ((i + j) != 0) {
-                if (i == 0) {
-                    j -= 1;
-                } else if (j == 0) {
-                    i -= 1;
-                } else {
-                    double[] array = { D[i - 1][j], D[i][j - 1], D[i - 1][j - 1] };
-                    minIndex = this.getIndexOfMinimum(array);
+        this.reversePath(warpingPath);
+    }
 
-                    if (minIndex == 0) {
-                        i -= 1;
-                    } else if (minIndex == 1) {
-                        j -= 1;
-                    } else if (minIndex == 2) {
-                        i -= 1;
-                        j -= 1;
-                    }
-                }
-                K++;
-                warpingPath[K - 1][0] = i;
-                warpingPath[K - 1][1] = j;
+    // Changes the order of the warping path (increasing order)
+    protected void reversePath(int[][] path) {
+        int[][] newPath = new int[K][2];
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < 2; j++) {
+                newPath[i][j] = path[K - i - 1][j];
             }
-            warpingDistance = accumulatedDistance / K;
-
-            this.reversePath(warpingPath);
         }
+        warpingPath = newPath;
+    }
 
-        // modifies the order of the warping path (increasing order)
-        protected void reversePath(int[][] path) {
-            int[][] newPath = new int[K][2];
-            for (int i = 0; i < K; i++) {
-                for (int j = 0; j < 2; j++) {
-                    newPath[i][j] = path[K - i - 1][j];
-                }
+    // Returns the warping distance
+    public double getDistance() {
+        return warpingDistance;
+    }
+
+    // Computes a distance between two points
+    protected double distanceBetween(double p1, double p2) {
+        return (p1 - p2) * (p1 - p2);
+    }
+
+    // Finds the index of the minimum element from the given array
+    protected int getIndexOfMinimum(double[] array) {
+        int index = 0;
+        double val = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < val) {
+                val = array[i];
+                index = i;
             }
-            warpingPath = newPath;
         }
+        return index;
+    }
 
-        // returns the warping distance
-        public double getDistance() {
-            return warpingDistance;
+
+    //	Returns a string that displays the warping distance and path
+
+    public String toString() {
+        String returnVal = "Warping Distance: " + warpingDistance + "\n";
+        returnVal += "Warping Path: {";
+        for (int i = 0; i < K; i++) {
+            returnVal += "(" + warpingPath[i][0] + ", " +warpingPath[i][1] + ")";
+            returnVal += (i == K - 1) ? "}" : ", ";
         }
-
-        // calculates the distance between two points
-        protected double distanceBetween(double p1, double p2) {
-            return (p1 - p2) * (p1 - p2);
-        }
-
-        // Finds the index of the min element from the given array
-        protected int getIndexOfMinimum(double[] array) {
-            int index = 0;
-            double val = array[0];
-
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < val) {
-                    val = array[i];
-                    index = i;
-                }
-            }
-            return index;
-        }
-
-
-        //	Returns a string that outputs the warping distance and the path
-        public String toString() {
-            String retVal = "Warping Distance: " + warpingDistance + "\n";
-            retVal += "Warping Path: {";
-            for (int i = 0; i < K; i++) {
-                retVal += "(" + warpingPath[i][0] + ", " + warpingPath[i][1] + ")";
-                retVal += (i == K - 1) ? "}" : ", ";
-
-            }
-            return retVal;
-        }
+        return returnVal;
+    }
 }
